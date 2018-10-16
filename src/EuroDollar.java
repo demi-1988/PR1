a40bdcce408fe58dc7e16b5944510d802fba2618
student <student@192.168.101.20>
2018-10-16 12:42:45 +0200

    Euro/Dollar umrechnen.

--- src/EuroDollar.java
+++ src/EuroDollar.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class EuroDollar {
 
 	public static void main(String[] args) {
-		// TODO Automatisch generierter Methodenstub
+		// EuroDollarUmrechnung
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Bitte geben Sie den Betrag in Euro/Dollar ein:");
+		float Betrag = sc.nextFloat();
 
+		double Euro = Betrag*1.34;
+		System.out.println("Der Betrag in Euro betraegt:"+Euro);
+	
+		int menue = 1;
+		if (menue == 1){
+			
+			
+		}else{
+		
+		}
+		
+		
+	}
 	}
 
-}
